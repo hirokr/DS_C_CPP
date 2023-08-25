9a9e8053214f6fa07454439491fd5aec10321fcc
Hirok Roy Rahul <hirokr@users.noreply.github.com>
2023-08-25 13:17:40 +0600

    pritier

--- Stack/first_stack.cpp
+++ Stack/first_stack.cpp
@@ -1,23 +1,27 @@
-#include<iostream>
-#include<stack> //standard template library
-#include<bits/stdc++.h>
+#include <iostream>
+#include <stack> //standard template library
+#include <bits/stdc++.h>
 using namespace std;
 
-void Reversed(char *C, int n){
+void Reversed(char *C, int n)
+{
     stack<char> S;
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         S.push(C[i]);
     }
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         C[i] = S.top();
         S.pop();
     }
 };
 
-int main(){
+int main()
+{
     char C[51];
     printf("Enter a string: ");
-    gets(C);   
+    gets(C);
     Reversed(C, strlen(C));
     printf("Output = %s", C);
 }
